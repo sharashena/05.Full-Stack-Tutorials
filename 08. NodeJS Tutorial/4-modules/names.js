7d98cd7b5c0074f261c318614f2526c75a61372e
Luka Sharashenidze <lsharashenidze2001@gmail.com>
2024-03-26 16:48:44 +0400

    Full-Stack Tutorials

---
+++ 08. NodeJS Tutorial/4-modules/names.js
@@ -0,0 +1,6 @@
+// share
+const john = "john";
+const peter = "peter";
+
+// if we want to share these variables or something else we use module
+module.exports = { john, peter };
---
+++ 08. NodeJS Tutorial/4-modules/utils.js
@@ -0,0 +1,6 @@
+const sayHi = name => {
+  console.log(`Hello there ${name}`);
+};
+
+// it's same as export default in JS
+module.exports = sayHi;
